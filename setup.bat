6810234ba6ccb04799656c1b0c9395a0036fabc5
eduardofae <eduardofaeef@gmail.com>
2025-05-19 03:50:35 -0300

    Atualização do arquivo de setup para corrigir automaticamente o erro em moverscore

--- setup.bat
+++ setup.bat
@@ -1,16 +1,51 @@
 @echo off
-git submodule init
-git submodule update
-python -m venv .venv
-(
-    echo @echo off
-    echo .venv\Scripts\activate 
-) > activate.bat
-call activate
-pip install -r requirements.txt
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
+
+call:pull_submodules
+call:create_venv
+call:create_exec
+
+:create_exec
 (
     echo @echo off
     echo call activate 
     echo python run.py
 ) > run.bat
+exit /b
+
+:create_venv
+python -m venv .venv
+call :create_activate
+call activate
+pip install -r requirements.txt
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
+exit /b
+
+:create_activate
+(
+    echo @echo off
+    echo .venv\Scripts\activate 
+) > activate.bat
+exit /b
+
+:pull_submodules
+git submodule init
+git submodule update
+call:fix_moverscore
+exit /b
+
+:fix_moverscore
+set "source=external\moverscore\moverscore_v2.py"
+set "temp=external\moverscore\moverscore_v2.temp"
+rename %source% "moverscore_v2.temp"
+setlocal enableDelayedExpansion
+(
+   for /F "tokens=1* delims=:" %%a in ('findstr /N "^" %temp%') do (
+      set "line=%%b"
+      if defined line set "line=!line:np.float=float!"
+      if defined line set "line=!line:float32=float!"
+      echo(!line!)
+   ) > %source%
+)
+endlocal
+del %temp%
+exit /b
