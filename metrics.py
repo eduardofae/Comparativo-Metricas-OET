c10fdb0b8b9847eafb4944b5a6a8e1eced9eccde
eduardofae <eduardofaeef@gmail.com>
2025-05-21 23:24:04 -0300

    Adição do BARTScore

--- metrics.py
+++ metrics.py
@@ -1,6 +1,7 @@
 import evaluate
 from external.moverscore.moverscore_v2 import word_mover_score
 from collections import defaultdict
+from external.BARTScore.bart_score import BARTScorer
 
 def add_bleu(prediction, reference, data):
     metric_name = 'bleu'
@@ -33,11 +34,18 @@ def add_moverscore(prediction, reference, data):
     results = word_mover_score([reference], [prediction], idf_dict_ref, idf_dict_hyp, stop_words=[], n_gram=1, remove_subwords=False)
     data[reference][metric_name] = results[0]
 
+def add_bartscore(prediction, reference, data):
+    metric_name = 'bartscore'
+    bart_scorer = BARTScorer(device='cuda:0', checkpoint='facebook/bart-large-cnn')
+    results = bart_scorer.score([prediction], [reference], batch_size=4)
+    data[reference][metric_name] = results[0]
+
 def add_metrics(prediction, reference, data, lang='en'):
     add_bleu(prediction, reference, data)
     #add_rouge(prediction, reference, data)
     add_meteor(prediction, reference, data)
     add_bertscore(prediction, reference, data, lang)
     add_moverscore(prediction, reference, data)
+    add_bartscore(prediction, reference, data)
 
 
