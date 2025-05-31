525af58f782d2e9740b93b791f89e8b2fb23a1e2
eduardofae <eduardofaeef@gmail.com>
2025-05-31 03:01:23 -0300

    Implementação do ROUGE e atualização do BERTScore

--- src/metrics.py
+++ src/metrics.py
@@ -1,7 +1,9 @@
 import evaluate
+from rouge import Rouge 
 from external.moverscore.moverscore_v2 import word_mover_score
 from collections import defaultdict
 from external.BARTScore.bart_score import BARTScorer
+
 
 def add_bleu(prediction, reference, data):
     metric_name = 'bleu'
@@ -17,15 +19,19 @@ def add_meteor(prediction, reference, data):
 
 def add_rouge(prediction, reference, data):
     metric_name = 'rouge'
-    metric = evaluate.load(metric_name)
-    results = metric.compute(predictions=[prediction], references=[reference])
-    data[reference][metric_name] = results[metric_name]
+    metric = Rouge()
+    results = metric.get_scores(hyps=prediction, refs=reference)
+    data[reference][metric_name+'-1'] = results[0][metric_name+'-1']['f']
+    data[reference][metric_name+'-2'] = results[0][metric_name+'-2']['f']
+    data[reference][metric_name+'-l'] = results[0][metric_name+'-l']['f']
 
 def add_bertscore(prediction, reference, data, lang):
     metric_name = 'bertscore'
     metric = evaluate.load(metric_name)
     results = metric.compute(predictions=[prediction], references=[reference], lang=lang)
-    data[reference][metric_name] = results['f1'][0]
+    data[reference][metric_name+'-p']  = results['precision'][0]
+    data[reference][metric_name+'-r']  = results['recall'][0]
+    data[reference][metric_name+'-f1'] = results['f1'][0]
 
 def add_moverscore(prediction, reference, data):
     metric_name = 'moverscore'
@@ -42,7 +48,7 @@ def add_bartscore(prediction, reference, data):
 
 def add_metrics(prediction, reference, data, lang='en'):
     add_bleu(prediction, reference, data)
-    #add_rouge(prediction, reference, data)
+    add_rouge(prediction, reference, data)
     add_meteor(prediction, reference, data)
     add_bertscore(prediction, reference, data, lang)
     add_moverscore(prediction, reference, data)
